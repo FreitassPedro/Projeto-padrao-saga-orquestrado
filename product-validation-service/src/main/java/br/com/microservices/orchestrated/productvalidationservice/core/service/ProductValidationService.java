0956ba913f7aa2fcfe5f1e90580f2a1d92b7e7d7
Pedro Freitas PC <pedrospf2018@gmail.com>
2024-05-27 15:04:34 -0300

    Finalizado Product-Validation-Service

---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/service/ProductValidationService.java
@@ -0,0 +1,134 @@
+package br.com.microservices.orchestrated.productvalidationservice.core.service;
+
+import br.com.microservices.orchestrated.productvalidationservice.config.exception.ValidationException;
+import br.com.microservices.orchestrated.productvalidationservice.core.dto.Event;
+import br.com.microservices.orchestrated.productvalidationservice.core.dto.History;
+import br.com.microservices.orchestrated.productvalidationservice.core.dto.OrderProducts;
+import br.com.microservices.orchestrated.productvalidationservice.core.enums.ESagaStatus;
+import br.com.microservices.orchestrated.productvalidationservice.core.model.Validation;
+import br.com.microservices.orchestrated.productvalidationservice.core.producer.KafkaProducer;
+import br.com.microservices.orchestrated.productvalidationservice.core.repository.ProductRepository;
+import br.com.microservices.orchestrated.productvalidationservice.core.repository.ValidationRepository;
+import br.com.microservices.orchestrated.productvalidationservice.core.utils.JsonUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+import static br.com.microservices.orchestrated.productvalidationservice.core.enums.ESagaStatus.*;
+import static org.springframework.util.ObjectUtils.isEmpty;
+
+@Slf4j
+@AllArgsConstructor
+@Service
+public class ProductValidationService {
+
+    private static final String CURRENT_SOURCE = "PRODUCT_VALIDATION_SERVICE";
+
+    private final JsonUtil jsonUtil;
+    private final KafkaProducer producer;
+    private final ProductRepository productRepository;
+    private final ValidationRepository validationRepository;
+
+
+    public void validateExistingProducts(Event event) {
+        try {
+            checkCurrentValidation(event);
+            createValidation(event, true);
+            handleSuccess(event);
+        } catch (Exception ex) {
+            log.error("Error trying to validate products: ", ex);
+            handleFailCurrentNotExecuted(event, ex.getMessage());
+        }
+        producer.sendEvent(jsonUtil.toJson(event));
+    }
+
+    private void checkCurrentValidation(Event event) {
+        validatePayloadListProductsInformed(event);
+        if (validationRepository.existsByOrderIdAndTransactionId(event.getOrderId(), event.getTransactionId())) {
+            throw new ValidationException("Another transaction already exists for this validation.");
+        }
+        event.getPayload().getProducts().forEach(product -> {
+            checkAllValidationsByProduct(product);
+        });
+
+    }
+
+    private static void validatePayloadListProductsInformed(Event event) {
+        if (isEmpty(event.getPayload()) || isEmpty(event.getPayload().getProducts())) {
+            throw new ValidationException("Product list is empty!");
+        }
+        if (isEmpty(event.getPayload().getId()) || isEmpty(event.getPayload().getTransactionId())) {
+            throw new ValidationException("OrderID and TransactionID must be informed!");
+        }
+    }
+
+    private void checkAllValidationsByProduct(OrderProducts product) {
+        validateProductInformed(product);
+        validateExistingProduct(product.getProduct().getCode());
+    }
+
+    private void validateExistingProduct(String code) {
+        if (!productRepository.existsByCode(code)) {
+            throw new ValidationException("Product does not exists in the database");
+        }
+    }
+
+    private void validateProductInformed(OrderProducts product) {
+        if (isEmpty(product.getProduct()) || isEmpty(product.getProduct().getCode())) {
+            throw new ValidationException("Product must be informed");
+        }
+    }
+
+    private void createValidation(Event event, boolean success) {
+        var validation = Validation.builder()
+                .orderId(event.getPayload().getId())
+                .transactionId(event.getTransactionId())
+                .success(success)
+                .build();
+
+        validationRepository.save(validation);
+    }
+
+    private void addHistory(Event event, String message) {
+        var history = History.builder()
+                .source(event.getSource())
+                .status(event.getStatus())
+                .message(message)
+                .createdAt(LocalDateTime.now())
+                .build();
+        event.addToHistory(history);
+    }
+
+    private void handleSuccess(Event event) {
+        event.setStatus(SUCCESS);
+        event.setSource(CURRENT_SOURCE);
+        addHistory(event, "Products have been successfully validated!");
+    }
+
+    private void handleFailCurrentNotExecuted(Event event, String message) {
+        event.setStatus(ROLLBACK_PENDING);
+        event.setSource(CURRENT_SOURCE);
+        addHistory(event, "Failure to validate products: ".concat(message));
+    }
+
+    public void rollbackEvent(Event event) {
+        changeValidationToFail(event);
+        event.setStatus(FAIL);
+        event.setSource(CURRENT_SOURCE);
+        addHistory(event, "Rollback performed during product validation");
+        producer.sendEvent(jsonUtil.toJson(event));
+    }
+
+    private void changeValidationToFail(Event event) {
+        validationRepository.findByOrderIdAndTransactionId(
+                        event.getPayload().getId(), event.getPayload().getTransactionId())
+                .ifPresentOrElse(validation -> {
+                            validation.setSuccess(false);
+                            validationRepository.save(validation);
+                        },
+                        () -> createValidation(event, false));
+    }
+
+}
